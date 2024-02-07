128528cdea6fa13bfafb281583322c36d3d336d7
Im Chaewon <lisa425@sookmyung.ac.kr>
2024-02-07 17:48:24 +0900

    무한스크롤 구현

--- src/pages/api/store.ts
+++ src/pages/api/store.ts
@@ -1,27 +1,36 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { StoreApiResponse } from "@/interface";
+import { StoreApiResponse, StoreType } from "@/interface";
 import { PrismaClient } from "@prisma/client";
 import { count } from "console";
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<StoreApiResponse>
+  res: NextApiResponse<StoreApiResponse | StoreType[]>
 ) {
-  const {page = "1"}:{page?:string} = req.query;
+  const {page = ""}:{page?:string} = req.query;
   const prisma = new PrismaClient();
   
-  const count = await prisma.store.count();
-  const skipPage = parseInt(page) - 1;
-  const data = await prisma.store.findMany({
-    orderBy:{id:'asc'},
-    take:10,
-    skip: skipPage * 10,
-  });
+  if(page){
+    const count = await prisma.store.count();
+    const skipPage = parseInt(page) - 1;
+    const data = await prisma.store.findMany({
+      orderBy:{id:'asc'},
+      take:10,
+      skip: skipPage * 10,
+    });
 
-  res.status(200).json({
-    page: parseInt(page),
-    data: data,
-    totalCount: count,
-    totalPage: Math.ceil(count / 10)
-  });
+    res.status(200).json({
+      page: parseInt(page),
+      data: data,
+      totalCount: count,
+      totalPage: Math.ceil(count / 10)
+    });
+  }else{
+    const stores = await prisma.store.findMany({
+      orderBy: {id:"asc"},
+    })
+    return res.status(200).json(stores)
+  }
+
+
 }
--- src/pages/stores/index.tsx
+++ src/pages/stores/index.tsx
@@ -1,76 +1,122 @@
-import { StoreApiResponse } from "@/interface";
-import Image from 'next/image';
+import React, { useRef, useEffect, useCallback } from "react";
+
+import Image from "next/image";
+import { StoreType } from "@/interface";
+
+import { useInfiniteQuery } from "react-query";
+
 import axios from "axios";
-import { useQuery } from "react-query";
 import Loading from "@/components/Loading";
 import { useRouter } from "next/router";
-import Link from "next/link";
-import Pagination from "@/components/Pagination";
 
-export default function StoreListPage(){
-    const router = useRouter();
-    const { page = '1' } : any = router.query;
+import useIntersectionObserver from "@/hooks/useIntersectionObserver";
+import Loader from "@/components/Loader";
 
-    const {isLoading, isError, data:stores} = useQuery(`stores-${page}`, async () => {
-        const { data } = await axios(`/api/store?page=${page}`)
-        return data as StoreApiResponse
-    })
+export default function StoreListPage() {
+  const router = useRouter();
+  const { page = "1" }: any = router.query;
+  const ref = useRef<HTMLDivElement | null>(null);
+  const pageRef = useIntersectionObserver(ref, {});
+  const isPageEnd = !!pageRef?.isIntersecting;
 
-    if(isError){
-        return <div className="w-full h-screen mx-auto pt-[30%] text-red-500 text-center font-semibold">다시 시도해주세요.</div>
+  const fetchStores = async ({ pageParam = 1 }) => {
+    const { data } = await axios("/api/store?page=" + pageParam, {
+      params: {
+        limit: 10,
+        page: pageParam,
+      },
+    });
+
+    return data;
+  };
+
+  const {
+    data: stores,
+    isFetching,
+    fetchNextPage,
+    isFetchingNextPage,
+    hasNextPage,
+    isError,
+    isLoading,
+  } = useInfiniteQuery("stores", fetchStores, {
+    getNextPageParam: (lastPage: any) =>
+      lastPage.data?.length > 0 ? lastPage.page + 1 : undefined,
+  });
+
+  const fetchNext = useCallback(async () => {
+    const res = await fetchNextPage();
+    if (res.isError) {
+      console.log(res.error);
+    }
+  }, [fetchNextPage]);
+
+  useEffect(() => {
+    let timerId: NodeJS.Timeout | undefined;
+
+    if (isPageEnd && hasNextPage) {
+      timerId = setTimeout(() => {
+        fetchNext();
+      }, 500);
     }
 
+    return () => clearTimeout(timerId);
+  }, [fetchNext, isPageEnd, hasNextPage]);
+
+  if (isError) {
     return (
-        <div className="px-4 md:max-w-4xl mx-auto py-8">
-            <ul role="list" className="divide-y divide-gray-100">
-                { isLoading 
-                    ? <Loading />
-                    : stores?.data.map((store, index) => (
-                        <li className="flex justify-between gap-x-6 py-5" key={index}>
-                            <div className="flex gap-x-4">
-                            <Image
-                                src={
-                                store?.category
-                                    ? `/images/markers/${store?.category}.png`
-                                    : "/images/markers/default.png"
-                                }
-                                width={48}
-                                height={48}
-                                alt="아이콘 이미지"
-                            />
-                            <div>
-                                <div className="text-sm font-semibold leading-6 text-gray-900">
-                                {store?.name}
-                                </div>
-                                <div className="mt-1 text-xs truncate font-semibold leading-5 text-gray-500">
-                                {store?.storeType}
-                                </div>
-                            </div>
-                            </div>
-                            <div className="hidden sm:flex sm:flex-col sm:items-end">
-                            <div className="text-sm font-semibold leading-6 text-gray-900">
-                                {store?.address}
-                            </div>
-                            <div className="mt-1 text-xs truncate font-semibold leading-5 text-gray-500">
-                                {store?.phone || "번호없음"} | {store?.foodCertifyName} |{" "}
-                                {store?.category}
-                            </div>
-                            </div>
-                        </li>
-                    ))
-                }
-            </ul>
-            {stores?.totalPage 
-              && <Pagination total={stores.totalPage} page={page}/>
-            }
-        </div>
+      <div className="w-full h-screen mx-auto pt-[10%] text-red-500 text-center font-semibold">
+        다시 시도해주세요
+      </div>
     );
+  }
+
+  return (
+    <div className="px-4 md:max-w-4xl mx-auto py-8">
+      <ul role="list" className="divide-y divide-gray-100">
+        {isLoading ? (
+          <Loading />
+        ) : (
+          stores?.pages?.map((page, index) => (
+            <React.Fragment key={index}>
+              {page.data.map((store: StoreType, idx:number) => (
+                <li className="flex justify-between gap-x-6 py-5" key={idx}>
+                  <div className="flex gap-x-4">
+                    <Image
+                      src={
+                        store?.category
+                          ? `/images/markers/${store?.category}.png`
+                          : "/images/markers/default.png"
+                      }
+                      width={48}
+                      height={48}
+                      alt="아이콘 이미지"
+                    />
+                    <div>
+                      <div className="text-sm font-semibold leading-6 text-gray-900">
+                        {store?.name}
+                      </div>
+                      <div className="mt-1 text-xs truncate font-semibold leading-5 text-gray-500">
+                        {store?.storeType}
+                      </div>
+                    </div>
+                  </div>
+                  <div className="hidden sm:flex sm:flex-col sm:items-end">
+                    <div className="text-sm font-semibold leading-6 text-gray-900">
+                      {store?.address}
+                    </div>
+                    <div className="mt-1 text-xs truncate font-semibold leading-5 text-gray-500">
+                      {store?.phone || "번호없음"} | {store?.foodCertifyName} |{" "}
+                      {store?.category}
+                    </div>
+                  </div>
+                </li>
+              ))}
+            </React.Fragment>
+          ))
+        )}
+      </ul>
+      {(isFetching || hasNextPage || isFetchingNextPage) && <Loader />}
+      <div className="w-full touch-none h-10 mb-10" ref={ref} />
+    </div>
+  );
 }
-
-// export async function getServerSideProps() {
-//   const stores = await axios(`${process.env.NEXT_PUBLIC_API_URL}/api/store`)
-
-//   return {
-//     props: { stores: stores.data },
-//   };
-// }
